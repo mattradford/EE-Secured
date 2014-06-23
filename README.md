cf3fd75833d2b66c3f19d84fb747b556b28372ce
mattradford <webform@mattrad.co.uk>
2014-06-23 13:34:05 +0100

    Added jail.local and fail2ban filter for WP

--- README.md
+++ README.md
@@ -9,12 +9,14 @@ The script:
 
 * updates APT
 * adds & enables 1GB swap
-* adds & enables UFW
-  * configures rules
-  * whitelists CloudFlare IPIP Ranges
 * configures unattended security updates
 * installs fail2ban
 * installs EasyEngine
+* changes default SSH port to a random one and allows only key-based login
+* adds & enables UFW
+  * configures rules, including randomly assigned SSH port
+  * whitelists CloudFlare IPv4 Ranges
+* prints the ssh command to access the new server
 
 In order to run the script, create a new droplet, SSH into it, then:
 
@@ -22,12 +24,12 @@ In order to run the script, create a new droplet, SSH into it, then:
 * `chmod +x do-install.sh`
 * `./do-install.sh`
 
-After the script has run, I then log in to the server again using a key & the new port and check everything is correct. Then I alter EasyEngine's conf file (`/etc/easyengine`).
+After the script has run, I then log in to the server again using a key & the new port and check everything is correct. Then I alter EasyEngine's conf file (`/etc/easyengine/ee.conf`) to suit my defaults.
 
 The server is then ready for `ee site create` command to start adding WordPress sites.
 
 ## To Do
-* change default SSH port and allow only key-based login
+* 
   * (assumes that a key has already been added to the server during droplet creation)
 * configure other fail2ban rules
 * alter default web page for server to show the hostname
--- do-install.sh
+++ do-install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 # server setup script for 512MB DO Droplet
 
 echo "Setting up server"
 
 # update package list
-apt-get update
-echo "Updated APT"
+echo "Updating APT & doing any system upgrades. Hang on..."
+apt-get update &>> /dev/null
 
 # add 1GB swap
 # https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-14-04
@@ -18,42 +18,6 @@ sudo echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 
 # swap done
 echo "Swap created and enabled"
-
-#install and configure ufw
-apt-get install ufw
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow 25002
-ufw allow www
-ufw allow https
-# allow outgoing mail only, not ufw allow mail
-ufw allow 25
-ufw allow mysql
-ufw allow smtp
-# allow DNS for Postfix lookups
-ufw allow 53
-
-#allow CloudFlare IPs
-ufw allow from 204.93.240.0/24
-ufw allow from 204.93.177.0/24
-ufw allow from 199.27.128.0/21
-ufw allow from 173.245.48.0/20
-ufw allow from 103.21.244.0/22
-ufw allow from 103.22.200.0/22
-ufw allow from 103.31.4.0/22
-ufw allow from 141.101.64.0/18
-ufw allow from 108.162.192.0/18
-ufw allow from 190.93.240.0/20
-ufw allow from 188.114.96.0/20
-ufw allow from 197.234.240.0/22
-ufw allow from 198.41.128.0/17
-ufw allow from 162.158.0.0/15
-
-# enable ufw
-ufw enable
-
-# ufw installed
-echo "UFW installed and enabled"
 
 # add nightly security updates
 # http://blog.vigilcode.com/2011/04/ubuntu-server-initial-security-quick-secure-setup-part-i/
@@ -104,5 +68,56 @@ service fail2ban restart
 curl -sL rt.cx/ee | sudo bash
 ee system install
 
+# alter SSH login 
+s5="Port 22"
+s6="Port "
+s7=$(shuf -i 2000-65000 -n 1)
+sed -i "s/$s5/$s6$s7/g" /etc/ssh/sshd_config
+s8="PermitRootLogin yes"
+s9="PermitRootLogin without-password"
+sed -i "s/$s8/$s9/g" /etc/ssh/sshd_config
+echo "AllowUsers root www-data" >> /etc/ssh/sshd_config
+
+#restart sshd
+service ssh restart
+
+#install and configure ufw
+apt-get install ufw
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow $s7
+ufw allow www
+ufw allow https
+# allow outgoing mail only, not ufw allow mail
+ufw allow 25
+ufw allow mysql
+ufw allow smtp
+# allow DNS for Postfix lookups
+ufw allow 53
+
+#allow CloudFlare IPs
+ufw allow from 204.93.240.0/24
+ufw allow from 204.93.177.0/24
+ufw allow from 199.27.128.0/21
+ufw allow from 173.245.48.0/20
+ufw allow from 103.21.244.0/22
+ufw allow from 103.22.200.0/22
+ufw allow from 103.31.4.0/22
+ufw allow from 141.101.64.0/18
+ufw allow from 108.162.192.0/18
+ufw allow from 190.93.240.0/20
+ufw allow from 188.114.96.0/20
+ufw allow from 197.234.240.0/22
+ufw allow from 198.41.128.0/17
+ufw allow from 162.158.0.0/15
+
+# enable ufw
+ufw enable
+
+# ufw installed
+echo "UFW installed and enabled"
+
 # and we're done!
-echo "All Done!"
+s10=$(hostname  -I | cut -f1 -d' ')
+echo "$(tput setaf 1)Now log in using a new terminal and the following command:$(tput sgr 0)"
+echo "$(tput setaf 1)ssh -p $s7 root@$s10$(tput sgr 0)"
